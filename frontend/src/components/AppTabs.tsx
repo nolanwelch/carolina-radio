dd5f61143b7cdc9cc1c9ba9e340e86600f6f8e32
gchuaunc <gabrianrchua@gmail.com>
2024-11-02 13:10:59 -0400

    feat: create main tabs component

---
+++ frontend/src/components/AppTabs.tsx
@@ -0,0 +1,50 @@
+import { Box, Tab, Tabs } from '@mui/material';
+import { useState } from 'react';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function AppTabs() {
+  const [value, setValue] = useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  
+  return (
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', bgcolor: 'blue' }}>
+        <Tabs variant="fullWidth" value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Next Up"/>
+          <Tab label="My Queue"/>
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Next up component here
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Queue component here
+      </TabPanel>
+    </>
+  );
+}
+
+export default AppTabs;
