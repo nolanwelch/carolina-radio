69df74834168196a4c2b0c65b45241090ef98235
krohan03 <47457574+rohankash@users.noreply.github.com>
2024-11-02 13:19:51 -0400

    API request

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
+import random
+import math
+import requests
+import base64
 import os
+from urllib.parse import urlencode
 from dataclasses import dataclass
 
 import dotenv
@@ -7,6 +12,14 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import pandas as pd
 
+from fastapi import FastAPI, HTTPException, Request, Response
+from fastapi.responses import RedirectResponse, HTMLResponse
+
+STATE_KEY = "spotify_auth_state"
+CLIENT_ID = "15d9c47ab81448a8b7491c83e65b0302"
+REDIRECT_URI = "https://api.carolinaradio.tech/callback"
+
+api = FastAPI()
 
 @dataclass
 class UserVote:
@@ -36,6 +49,70 @@ def main():
     votes = [UserVote(1, "test"), UserVote(1, "test2"), UserVote(2, "test3")]
     print(choose_next_song(votes))
 
+@api.get("/login")
+def read_root():
+    state = generate_random_string(20)
+    scope = ""
+
+    params = {
+        "response_type": "code",
+        "client_id": CLIENT_ID,
+        "scope": scope,
+        "redirect_uri": REDIRECT_URI,
+        "state": state,
+    }
+    response = RedirectResponse(
+        url="https://accounts.spotify.com/authorize?" + urlencode(params)
+    )
+    return response, state
+
+@api.get("/callback")
+def callback(request: Request, response: Response, stored_state: str):
+
+    code = request.query_params["code"]
+    state = request.query_params["state"]
+
+    if state == None or state != stored_state:
+        raise HTTPException(status_code=400, detail="State mismatch")
+    else:
+
+        url = "https://accounts.spotify.com/api/token"
+        request_string = CLIENT_ID + ":" + CLIENT_SECRET
+        encoded_bytes = base64.b64encode(request_string.encode("utf-8"))
+        encoded_string = str(encoded_bytes, "utf-8")
+        header = {
+            "content-type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic " + encoded_string
+            }
+
+        form_data = {
+            "code": code,
+            "redirect_uri": REDIRECT_URI,
+            "grant_type": "authorization_code",
+        }
+
+        api_response = requests.post(url, data=form_data, headers=header)
+
+        if api_response.status_code == 200:
+            data = api_response.json()
+            access_token = data["access_token"]
+            refresh_token = data["refresh_token"]
+
+            response = RedirectResponse(url=URI)
+            response.set_cookie(key="accessToken", value=access_token)
+            response.set_cookie(key="refreshToken", value=refresh_token)
+
+        return response
+
+def generate_random_string(string_length):
+    possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    text = "".join(
+        [
+            possible[math.floor(random.random() * len(possible))]
+            for i in range(string_length)
+        ]
+    )
+    return text
 
 if __name__ == "__main__":
     main()
