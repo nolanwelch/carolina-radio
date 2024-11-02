03596cfaf91eaeab7bd05a63707bed76a15bb483
gchuaunc <gabrianrchua@gmail.com>
2024-11-02 13:49:48 -0400

    feat: add style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
+import { CssBaseline } from '@mui/material';
 import './App.css';
 import AppTabs from './components/AppTabs';
 import SongPlaying from './components/SongPlaying';
+import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#7BAFD4',
+    },
+    secondary: {
+      main: '#C4D600',
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <SongPlaying />
       <AppTabs />
-    </>
+    </ThemeProvider>
   );
 }
 
