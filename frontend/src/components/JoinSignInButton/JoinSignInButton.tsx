b7eef7985d1942e8e72ec3b4b1ea6ede9100ec07
gchuaunc <gabrianrchua@gmail.com>
2024-11-02 17:35:33 -0400

    feat: add log in check via cookies

--- frontend/src/components/JoinSignInButton/JoinSignInButton.tsx
+++ frontend/src/components/JoinSignInButton/JoinSignInButton.tsx
@@ -1,10 +1,16 @@
-import { PlayArrow } from "@mui/icons-material";
+import { Login, PlayArrow } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import Cookies from "js-cookie";
 
 export default function JoinSignInButton() {
-  return (
+  const isSignedIn: boolean = Cookies.get("accessToken") ? true : false;
+
+  return isSignedIn ?
     <Button variant="contained" startIcon={<PlayArrow />}>
-      Sign in to Join
+      Join Radio
     </Button>
-  );
+    :
+    <Button variant="contained" startIcon={<Login />}>
+      Sign in to Join
+    </Button >
 }
