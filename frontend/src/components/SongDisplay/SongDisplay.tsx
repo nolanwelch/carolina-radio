7c84fde06800dd45d62783371fa113be82c2adbd
David Foss <david@fosshome.com>
2024-11-03 05:41:54 -0500

    add zero padding

--- frontend/src/components/SongDisplay/SongDisplay.tsx
+++ frontend/src/components/SongDisplay/SongDisplay.tsx
@@ -3,6 +3,12 @@ import styles from "./SongDisplay.module.css";
 import Song from '../../types/Song';
 
 export default function SongDisplay(props: {song: Song}) {
+  function pad(num: number, size: number) {
+    let s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
   return (
     <Card className={styles.cardContent}>
       <div className={styles.albumCover}><img src={props.song.coverUrl} /></div>
@@ -10,7 +16,7 @@ export default function SongDisplay(props: {song: Song}) {
         <Typography className={styles.songTitle}>{props.song.title}</Typography>
         <Typography className={styles.artistName}>{props.song.artists.join(", ")}</Typography>
       </div>
-      <Typography className={styles.songLength}>{Math.floor(props.song.durationMs / 1000 / 60)}:{Math.floor(props.song.durationMs / 1000 % 60)}</Typography>
+      <Typography className={styles.songLength}>{Math.floor(props.song.durationMs / 1000 / 60)}:{pad(Math.floor(props.song.durationMs / 1000 % 60), 2)}</Typography>
       {/*<Typography className={styles.requestCount}>Requested by {props.song.requestCount} users</Typography>*/}
     </Card>
   );
